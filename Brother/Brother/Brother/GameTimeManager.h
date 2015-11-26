969ba8e36e8dc31a462b2376b06216818077a5ed
Moto0116 <moto01160902@gmail.com>
2015-11-26 10:50:21 +0000

    TimeManagerをGametimeManagerに修正

---
+++ Brother/Brother/Brother/GameTimeManager.h
@@ -0,0 +1,26 @@
+#ifndef GAMETIMEMANAGER_H
+#define GAMETIMEMANAGER_H
+
+
+class ModeManager;
+enum GAMEMODE_NUM;
+
+
+class GameTimeManager
+{
+private:
+	ModeManager*		m_pModeManager;
+	GAMEMODE_NUM		m_CurrentMode;
+	unsigned int&				m_GameTime;
+
+public:
+	GameTimeManager(unsigned int& GameTime);
+	~GameTimeManager();
+	void Control();
+	void ModeManagerSet(ModeManager* pModeManager);
+	inline int GetGameTime();
+
+};
+
+
+#endif
